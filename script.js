9e6175d6c744126b1f439a656f2dc9b0bf07eb8a
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-05 20:56:34 +0200

    Implement gameController factory function

--- script.js
+++ script.js
@@ -160,3 +160,178 @@ const minimaxAiLogic = ((percentage) => {
         setAiPercentage
     };
 })(0);
+
+const gameController = (() => {
+    const _humanPlayer = Player('X');
+    const _aiPlayer = Player('O');
+    const _aiLogic = minimaxAiLogic;
+
+    const getHumanPlayer = () => _humanPlayer;
+    const getAiPlayer = () => _aiPlayer;
+
+    const _sleep = (ms) => {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    };
+
+    const _checkForRows = (board) => {
+        for (let i = 0; i < 3; i++) {
+            let row = [];
+            for (let j = i*3; j < i*3+3; j++) {
+                row.push(board.getField(j));
+            }
+            if (row.every(field => field == 'X') || row.every(field => field == 'O')) {
+                return true;
+            }
+        }
+        return false;
+    };
+    const _checkForColumns = (board) => {
+        for (let i = 0; i < 3; i++) {
+            let column = [];
+            for (let j = 0; j < 3; j++) {
+                column.push(board.getField(i+3*j));
+            }
+            if (column.every(field => field == 'X') || column.every(field => field == 'O')) {
+                return true;
+            }
+        }
+        return false;
+    };
+    const _checkForDiagonals = (board) => {
+        let diagonal1 = [board.getField(0), board.getField(4), board.getField(8)];
+        let diagonal2 = [board.getField(6), board.getField(4), board.getField(2)];
+        if (diagonal1.every(field => field == 'X') || diagonal1.every(field => field == 'O')) {
+            return true;
+        } else if (diagonal2.every(field => field == 'X') || diagonal2.every(field => field == 'O')) {
+            return true;
+        }
+        return false;
+    };
+
+    const checkForWin = (board) => {
+        if (_checkForRows(board) || _checkForColumns(board) || _checkForDiagonals(board)) {
+            return true;
+        }
+        return false;
+    };
+    const checkForDraw = (board) => {
+        if (checkForWin(board)) {
+            return false;
+        }
+        for (let i = 0; i < 9; i++) {
+            const field = board.getField(i);
+            if (field == undefined) {
+                return false;
+            }
+        }
+        return true;
+    };
+    const changeSign = (sign) => {
+        if (sign == 'X') {
+            _humanPlayer.setSign('X', true);
+            _aiPlayer.setSign('O');
+        } else if (sign == 'O') {
+            _humanPlayer.setSign('O', true);
+            _aiPlayer.setSign('X');
+        } else throw "Incorrect sign";
+    };
+    const playerStep = (num) => {
+        const field = gameBoard.getField(num);
+        if (field == undefined) {
+            gameBoard.setField(num, _humanPlayer);
+            if (checkForWin(gameBoard)) {
+                (async () => {
+                    await _sleep(500 + (Math.random() * 500));
+                    endGame(_humanPlayer.getSign());
+                })();
+            } else if (checkForDraw(gameBoard)) {
+                (async () => {
+                    await _sleep(500 + (Math.random() * 500));
+                    endGame("Draw");
+                })();
+            } else {
+                displayController.deactivate();
+                (async () => {
+                    await _sleep(250 + (Math.random() * 300));
+                    aiStep();
+                    if (!checkForWin(gameBoard)) {
+                        displayController.activate();
+                    }
+                })();
+            }
+        } else {
+            console.log("Already Filled");
+        }
+    };
+    const endGame = function(sign) {
+        const card = document.querySelector(".card");
+        card.classList.remove("unblur");
+        card.classList.add("blur");
+
+        const winElements = document.querySelectorAll(".win p");
+
+        if (sign == "Draw") {
+            winElements[3].classList.remove("hide");
+            console.log("It's a Draw");
+        } else {
+            console.log(`The winner is player ${sign}`);
+            winElements[0].classList.remove("hide");
+            if (sign.toLowerCase() == 'x') {
+                winElements[1].classList.remove("hide");
+            } else {
+                winElements[2].classList.remove("hide");
+            }
+        }
+        console.log("deactivate");
+        displayController.deactivate();
+        displayController.makeBodyRestart();
+    };
+    const aiStep = () => {
+        const num = _aiLogic.chooseField();
+        gameBoard.setField(num, _aiPlayer);
+        if (checkForWin(gameBoard)) {
+            (async () => {
+                await _sleep(500 + (Math.random() * 500));
+                endGame(_aiPlayer.getSign());
+            })();
+        } else if (checkForDraw(gameBoard)) {
+            (async () => {
+                await _sleep(500 + (Math.random() * 500));
+                endGame("Draw");
+            })();
+        }
+    };
+    const restart = async function() {
+        const card = document.querySelector(".card");
+        const winElements = document.querySelectorAll(".win p");
+
+        card.classList.add("unblur");
+
+        gameBoard.clear();
+        displayController.clear();
+        if (_humanPlayer.getSign() == 'O') {
+            aiStep();
+        }
+        console.log("restart");
+        console.log(minimaxAiLogic.getAiPercentage());
+        displayController.activate();
+
+        card.classList.remove("blur");
+
+        winElements.forEach(element => {
+            element.classList.add("hide");
+        });
+        document.body.removeEventListener("click", gameController.restart);
+    };
+
+    return {
+        getHumanPlayer,
+        getAiPlayer,
+        checkForWin,
+        checkForDraw,
+        changeSign,
+        playerStep,
+        endGame,
+        restart
+    };
+})();
