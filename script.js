35758bb0560a6c5f35ca34c01daf835644ec8ab9
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-05 20:18:55 +0200

    Implement minimaxAiLogic factory function

--- script.js
+++ script.js
@@ -65,3 +65,98 @@ const Player = (sign) => {
         setSign
     };
 };
+
+const minimaxAiLogic = ((percentage) => {
+    let aiPrecision = percentage;
+
+    const setAiPercentage = (oercentage) => {
+        aiPrecision = percentage;
+    };
+    const getAiPercentage = () => {
+        return aiPrecision;
+    };
+
+    const chooseField = () => {
+        const value = Math.floor(Math.random() * (100 + 1));
+
+        let choice = null;
+        if (value <= aiPrecision) {
+            console.log("bestChoice");
+            choice = minimax(gameBoard, gameController.getAiPlayer()).index;
+            const field = gameBoard.getField(choice);
+            if (field != undefined) {
+                return "error";
+            }
+        } else {
+            console.log("NotbestChoice");
+            const emptyFieldsIdx = gameBoard.getEmptyFieldsIdx();
+            let noBestMove = Math.floor(Math.random() * emptyFieldsIdx.length);
+            choice = emptyFieldsIdx[noBestMove];
+        }
+        return choice;
+    };
+    const findBestMove = (moves, palyer) => {
+        let bestMove;
+        if (player === gameController.getAiPlayer()) {
+            let bestScore = -10000;
+            for (let i = 0; i < moves.length; i++) {
+                if (moves[i].score > bestScore) {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        } else {
+            let bestScore = 10000;
+            for (let i = 0; i < moves.length; i++) {
+                if (moves[i].score < bestScore) {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        }
+        return moves[bestMove];
+    };
+    const minimax = (newBoard, player) => {
+        let empty = newBoard.getEmptyFieldsIdx();
+
+        if (gameController.checkForDraw(newBoard)) {
+            return { score: 0 };
+        } else if (gameController.checkForWin(newBoard)) {
+            if (player.getSign() == gameController.getHumanPlayer().getSign()) {
+                return { score: 10 };
+            } else if (player.getSign() == gameController.getAiPlayer().getSign()) {
+                return { score: -10 };
+            }
+        }
+
+        let moves = [];
+
+        for (let i = 0; i < empty.length; i++) {
+            let move = {};
+            move.index = empty[i];
+
+            newBoard.setFieldForAiLogic(empty[i], player);
+
+            if (player.getSign() == gameController.getAiPlayer().getSign()) {
+                let result = minimax(newBoard, gameController.getHumanPlayer());
+                move.score = result.score;
+            } else {
+                let result = minimax(newBoard, gameController.getAiPlayer());
+                move.score = result.score;
+            }
+
+            newBoard.setFieldForAiLogic(empty[i], undefined);
+
+            moves.push(move);
+        }
+        
+        return findBestMove(moves, player);
+    };
+
+    return {
+        minimax,
+        chooseField,
+        getAiPercentage,
+        setAiPercentage
+    };
+})(0);
